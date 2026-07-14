4afefa0b4461cfeb538503e55b86d08f6e35f3cc
Saharniu_Chelovek <Ssannya1177@bk.ru>
2026-07-15 02:26:53 +0300

    delete unused imports

--- server/src/test/java/ru/practicum/shareit/ErrorHandlerTest.java
+++ server/src/test/java/ru/practicum/shareit/ErrorHandlerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.error.exception.ConflictException;
 import ru.practicum.shareit.error.exception.ForbiddenException;
@@ -16,7 +15,6 @@ import ru.practicum.shareit.user.service.UserService;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
